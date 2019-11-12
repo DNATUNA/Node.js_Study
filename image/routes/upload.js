93a73fef35e56db340ae705039a04e52c7bdae58
chooeeh <chchgml10@gmail.com>
2019-11-13 00:30:03 +0900

    11/13

--- image/routes/upload.js
+++ image/routes/upload.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'}); // 파일 저장 위치를 dest 옵션으로 지정해주기
+const router = express.Router();
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('join', {
-        title: '회원가입 - 플리마켓',
-        user: null,
-        joinError: req.flash('joinError'),
-    });
+// 127.0.0.1:3000/upload로 들어오면 실행됨 (여기서 upload는 라우터 js파일)
+router.get('/', function (req, res, next) {
+     // render의 인자로는 띄워줄 views 폴더 안의 pug파일을 지정해준다.
+    res.render('uploadform');
 });
-router.post('/load', function(req, res, next) {
-    res.send('respond with a resource');
+
+router.post('/submit', upload.single('userfile'), function (req, res, next) {
+    console.log(req.file);
+    //res.send('업로드된 파일이름 : ' + req.file.filename);
+    res.render('imgshow');
 });
 
 module.exports = router;
