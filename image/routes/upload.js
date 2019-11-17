bead9f190dcba035e079d2da67e6166eb1259703
chooeeh <chchgml10@gmail.com>
2019-11-17 16:10:47 +0900

    11/17

--- image/routes/upload.js
+++ image/routes/upload.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'}); // 파일 저장 위치를 dest 옵션으로 지정해주기
 const router = express.Router();
+/* 파일 저장 위치 지정 */ 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({storage: storage});
 
 
-/* GET users listing. */
+/* get, post 코드 */
 // 127.0.0.1:3000/upload로 들어오면 실행됨 (여기서 upload는 라우터 js파일)
 router.get('/', function (req, res, next) {
      // render의 인자로는 띄워줄 views 폴더 안의 pug파일을 지정해준다.
     res.render('uploadform');
 });
 
-router.post('/submit', upload.single('userfile'), function (req, res, next) {
+// 최대 10개 파일을 한번에 보낼 수 있음
+router.post('/submit', upload.array('userfile',10), function (req, res, next) {
     console.log(req.file);
-    //res.send('업로드된 파일이름 : ' + req.file.filename);
-    res.render('imgshow');
 });
 
 module.exports = router;
