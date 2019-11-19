f9ebae98d4556f6a3bbb698160c6160d0c72332b
chooeeh <chchgml10@gmail.com>
2019-11-19 23:04:16 +0900

    11/19

--- image/routes/upload.js
+++ image/routes/upload.js
@@ -1,8 +1,27 @@
 var express = require('express');
+const multer = require('multer');
 var router = express.Router();
+/* 파일 저장 위치 지정 */ 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+      cb(null, file.originalname);
+  }
+});
+const upload = multer({storage: storage});
+
+
 
 router.get('/', function(req, res, next) {
-  res.render('upload');
+  res.render('upload_product');
+});
+
+// 최대 10개 파일을 한번에 보낼 수 있음
+router.post('/', upload.array('userfile',10), function (req, res, next) {
+  //console.log(req.file);
+  res.render('main');
 });
 
 module.exports = router;
