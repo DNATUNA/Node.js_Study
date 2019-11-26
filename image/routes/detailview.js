fc3a2e4f4ce1c34a39f7821b0851f0bc90d4615e
chooeeh <chchgml10@gmail.com>
2019-11-26 10:40:54 +0900

    no message

--- image/routes/detailview.js
+++ image/routes/detailview.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('detail_test');
+
+  // res.render('detail_view');
 });
 
 module.exports = router;
